0366f06be22503704f98b3d9afdbe81501634c56
jvpigozzo <jvpigozzo@gmail.com>
2024-05-04 15:38:15 -0300

    add item schema

---
+++ app/schemas/item.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+
+class ItemSchema(BaseModel):
+    name: str
+    label: str
+    # image_url: str
+
+
+class ItemCreate(ItemSchema):
+    pass
+
+
+def serialize_item(item):
+    item["id"] = str(item["_id"])
+    item.pop("_id")
+    return item
+
+
+def serialize_items(items):
+    return [serialize_item(item) for item in items]
+
+
+def serialize_dict(entity) -> dict:
+    if entity != None:
+        return {
+            **{i: str(entity[i]) for i in entity if i == "_id"},
+            **{i: entity[i] for i in entity if i != "_id"},
+        }
+    return None
